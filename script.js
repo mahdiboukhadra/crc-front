03fa8f1643c49c5785e0c7fc55e0b70ebc9a3682
mahdiboukhadra <bocamahdi@gmail.com>
2023-10-24 22:56:55 +0100

    x

---
+++ script.js
@@ -0,0 +1,19 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const endpointUrl = "https://hshyllcxpyt6bd5v3xuqqmezsy0ecnmd.lambda-url.eu-west-3.on.aws/"; // my lambda endpoint URL
+
+    fetch(endpointUrl)
+        .then((response) => {
+            if (!response.ok) {
+                throw new Error("Network response was not ok");
+            }
+            return response.json(); // since the response is in JSON format
+        })
+        .then((data) => {
+            // displaying the fetched data in the "data-container" div
+            const dataContainer = document.getElementById("data-container");
+            dataContainer.textContent = "This website has been visited " + JSON.stringify(data, null, 2) + " times";
+        })
+        .catch((error) => {
+            console.error("Error fetching data:", error);
+        });
+});
